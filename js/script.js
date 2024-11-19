797889f06b533c8469123d7b5a2b4516c99ea90f
Marco Mechini <mechinimarco35@gmail.com>
2024-11-19 15:23:21 +0100

    feat: milestone 3 complete

--- js/script.js
+++ js/script.js
@@ -3,14 +3,7 @@ const $one = (bind) => document.querySelector(bind);
 const album = $one('.album');
 console.log(album);
 
-// {albumId: 1, 
-// id: 1, 
-// title: 'accusamus beatae ad facilis cum similique qui sunt', 
-// url: 'https://via.placeholder.com/600/92c952', 
-// thumbnailUrl: 'https://via.placeholder.com/150/92c952'}
-
 let postArr = [];
-
 
 axios
     .get('https://jsonplaceholder.typicode.com/photos?_limit=6', { timeout: 8000 })
