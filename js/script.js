785920121de3bdc530ea62d00edd08b8458ecbb8
Marco Mechini <mechinimarco35@gmail.com>
2024-11-20 15:15:36 +0100

    feat: overlay styled, try to remove overlay on click all over the img

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 const $one = document.querySelector.bind(document);
 const $elem = document.createElement.bind(document);
 
+const body = $one('body');
 const album = $one('.album');
 const overlay = $one('.overlay');
 const overlayImg = $one('.overlay img');
-const exit = $one('.exit');
+
+//old overlay version
+//const exit = $one('.exit');
+//select all parents without child img tag 
+//now this select all the parents
+const exitAll = $one('div.overlay:not(img)');
 // const garbageBtn = $one('#garbage');
 // console.log(album);
 
@@ -30,30 +36,33 @@ const getPost = (posts) => {
         //     </div>
         //     `
 
+        //Create new element to show in page
         const post = $elem('div');
         const garbageBtn = $elem('i');
         const postImg = $elem('img');
         // const overlayImg = $elem('img');
         const postP = $elem('p');
 
+        //Add/Set class/attributs to the new element
         post.classList.add("post");
         garbageBtn.classList.add("fa-regular", "fa-trash-can", "garbage");
         postImg.src = `${curPost.url}`;
         postImg.alt = `${curPost.thumbnailUrl}`;
+        postP.innerText = `${curPost.title}`;
         // overlayImg.src = `${curPost.url}`;
         // overlayImg.alt = `${curPost.thumbnailUrl}`;
-        postP.innerText = `${curPost.title}`;
 
-
+        //funzione per rimuovere l'elemento al click su icona cestino
         garbageBtn.addEventListener('click', () => {
             console.log('sto per cancellare');
             post.remove();
         });
 
+        //click function show img in a page overlay
         postImg.addEventListener('click', () => {
-            console.log('over');
             overlay.classList.remove('d-none');
-            console.log(overlayImg);
+            //clas to set overflow: hidden
+            body.classList.add("stop-scroll")
             overlayImg.src = `${curPost.url}`;
             overlayImg.alt = `${curPost.thumbnailUrl}`;
         });
@@ -64,8 +73,9 @@ const getPost = (posts) => {
     })
 }
 
-exit.addEventListener('click', () => {
-    console.log('fine overlay');
+//click function all over img to exit from overlay
+exitAll.addEventListener('click', () => {
+    console.log('fine tutto');
 
     overlay.classList.add('d-none');
 })
